fe18685c218dd7d4525838aa532934c42a601b8e
SJS445 <sjs445@jupiter.acad.fullerton.edu>
2020-04-10 11:43:17 -0700

    AES progress

--- AES.cpp
+++ AES.cpp
@@ -1,5 +1,4 @@
 #include "AES.h"
-
 /**
  * Sets the key to use
  * @param key - the first byte of this represents whether
@@ -9,8 +8,49 @@
  * @return - True if the key is valid and False otherwise
  */
 bool AES::setKey(const unsigned char* keyArray)
-{
+{	
+	// Assuming we add 00 to the beginning of keyArray in cipher.cpp
 	
+	// if keyArray's size is 17 proceed
+	if(strlen((char *)keyArray)!=17)
+	{
+		fprintf(stderr, "Key should be 128 bits!\n");
+		return false;
+	}
+
+	unsigned char * hexArray=(unsigned char *)keyArray;
+
+	// convert keyArray to the hex array
+	for (int i = 0; i < 17; i+=2)
+	{
+		hexArray[i]=twoCharToHexByte((unsigned char *)keyArray[i]);
+	}
+
+	// Removing the first byte 
+	unsigned char * originalKey;
+	strncpy((char *)originalKey, (char*)keyArray+1, 16);
+
+	if(keyArray[0]==0x00)
+	{
+		// Set the encryption key	
+		if(AES_set_encrypt_key(originalKey, 128, &enc_key)!=0)
+		{
+			fprintf(stderr, "AES_set_encrypt_key() failed!\n");
+			return false;
+		}
+		return true;	
+	}
+	else	// Otherwise we use set_decrypt_key
+	{
+		//	Set the decryption key
+		if(AES_set_decrypt_key(originalKey, 128, &dec_key)!=0)
+		{
+			fprintf(stderr, "AES_SET_decrypt_key() failed!\n");
+			return false;
+		}
+		return true;
+	}
+		
 	// TODO: AES implementation of openssl cares about whether
 	// you are encrypting or decrypting when setting the key.
 	// That is, when encrypting you use function AES_set_encrypt_key(...)
@@ -40,11 +80,13 @@ unsigned char* AES::encrypt(const unsigned char* plainText)
 {
 	
 	//TODO: 1. Dynamically allocate a block to store the ciphertext.
+	unsigned char * enc_out = new unsigned char [16];
 	//	2. Use AES_ecb_encrypt(...) to encrypt the text (please see the URL in setKey(...)
 	//	and the aes.cpp example provided.
+	AES_ecb_encrypt(plainText, enc_out, &enc_key, AES_ENCRYPT);
 	// 	3. Return the pointer to the ciphertext
-		
-	return NULL;	
+
+	return enc_out;	
 }
 
 /**
@@ -56,12 +98,61 @@ unsigned char* AES::decrypt(const unsigned char* cipherText)
 {
 	
 	//TODO: 1. Dynamically allocate a block to store the plaintext.
+	unsigned char* dec_out = new unsigned char [16];
 	//	2. Use AES_ecb_encrypt(...) to decrypt the text (please see the URL in setKey(...)
 	//	and the aes.cpp example provided.
+	AES_ecb_encrypt(cipherText, dec_out, &dec_key, AES_DECRYPT);
 	// 	3. Return the pointer to the plaintext
 		
-	return NULL;
+	return dec_out;
+}
+
+/**
+ * Converts a character into a hexidecimal integer
+ * @param character - the character to convert
+ * @return - the converted character, or 'z' on error
+ */
+unsigned char AES::charToHex(const char& character)
+{
+	/* Is the first digit 0-9 ? */	
+	if(character >= '0' && character <= '9')	
+		/* Convert the character to hex */
+		return character - '0';
+	/* It the first digit a letter 'a' - 'f'? */
+	else if(character >= 'a' && character <= 'f')
+		/* Conver the cgaracter to hex */
+		return (character - 97) + 10;	
+	/* Invalid character */
+	else return 'z';
 }
 
 
+unsigned char AES::twoCharToHexByte(const unsigned char* twoChars)
+{
+	/* The byte */
+	unsigned char singleByte;
+	
+	/* The second character */
+	unsigned char secondChar;
 
+	/* Convert the first character */
+	if((singleByte = charToHex(twoChars[0])) == 'z') 
+	{
+		/* Invalid digit */
+		return 'z';
+	}
+	
+	/* Move the newly inserted nibble from the
+	 * lower to upper nibble.
+	 */
+	singleByte = (singleByte << 4);
+	
+	/* Conver the second character */
+	if((secondChar = charToHex(twoChars[1])) == 'z')
+		return 'z'; 
+	
+	/* Insert the second value into the lower nibble */	
+	singleByte |= secondChar;
+
+	return singleByte;
+}
